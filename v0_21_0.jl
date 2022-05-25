92841d3c8ba5317ae3bba18b0ac69dfc95efacb0
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-05-25 05:15:38 -0700

    Create v0_21_0.jl
    artifact_hash = SHA1("776d9adfdef4193324ab83c42b02d8b4b72389b0")

---
+++ v0_21_0.jl
@@ -0,0 +1,47 @@
+# awscli automatically installs python. So this isn't a python-less rootfs-image
+
+using RootfsUtils: parse_build_args, upload_gha, test_sandbox
+using RootfsUtils: debootstrap
+using RootfsUtils: root_chroot
+release      = "bullseye"
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "bash",
+    "locales",
+    "git",
+
+    "curl",
+    "vim",
+    "lldb",
+
+    "libblas-dev",
+    "liblapack-dev",
+
+    # Get a C compiler, for compiling python extensions
+    "build-essential",
+    # Get latex, so that we can invoke `pdflatex` and friends
+    "texlive-latex-extra",
+    "awscli"
+]
+
+# artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages, release)
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages, release) do rootfs, chroot_ENV
+    my_chroot(args...) = root_chroot(rootfs, "bash", "-c", args...; ENV=chroot_ENV)
+
+    @info("fixing systemd...")
+    dpkg_fix_cmd = """
+    dpkg --configure -a
+    apt-get update
+    apt-get upgrade
+    apt-get -y install gdb
+    """
+    my_chroot(dpkg_fix_cmd)
+end
+
+test_sandbox(artifact_hash)
