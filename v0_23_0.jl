451ffcbff5a3c16420798548cdcac721b7c1a56e
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-07-26 23:06:21 -0700

    Create v0_23_0.jl

---
+++ v0_23_0.jl
@@ -0,0 +1,29 @@
+# Adds `g++` to https://github.com/JuliaCI/rootfs-images/blob/main/linux/aws_uploader.jl
+
+using RootfsUtils: parse_build_args, upload_gha, test_sandbox
+using RootfsUtils: debootstrap
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+# Build debian-based image with the following extra packages:
+packages = [
+    "awscli",
+    "bash",
+    "curl",
+    "git",
+    "gpg",
+    "gpg-agent",
+    "locales",
+    "localepurge",
+    "make",
+    "openssh-client",
+    "vim",
+    "g++"
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages)
+upload_gha(tarball_path)
+test_sandbox(artifact_hash)
